1a4030c1f8c6b591f8104ff86d20294c99ed540c
Jaxlying <Jaxlying@outlook.com>
2016-07-17 23:08:47 +0800

    先增插入用户表

--- server/sql.cpp
+++ server/sql.cpp
@@ -6,7 +6,31 @@
 
 sql::sql()
 {
-    db = new QSqlDatabase;
-    db->addDatabase("QSQLITE");
+    db = new QSqlDatabase(QSqlDatabase::addDatabase("QSQLITE"));
     db->setDatabaseName("../bank.sql");
+    query = new QSqlQuery;
+    if (!db.open()) {
+        QMessageBox::critical(0, "Cannot open database",
+            "Unable to establish a database connection.", QMessageBox::Cancel);
+    }
+}
+bool sql::creatUser(int isvip)
+{
+    qDebug<<isvip;
+    query.exec("create table user (id INTEGER PRIMARY KEY, "
+               "isvip int,"
+               "isdeal int,"
+               "remark int)");
+    query.exec("insert into user(isvip,isdeal) values('0','0')");
+    return true;
+}
+bool sql::creatUser()
+{
+    qDebug<<isvip;
+    query.exec("create table user (id INTEGER PRIMARY KEY, "
+               "isvip int,"
+               "isdeal int,"
+               "remark int)");
+    query.exec("insert into user(isvip,isdeal) values('1','0')");
+    return true;
 }
--- server/sql.h
+++ server/sql.h
@@ -1,13 +1,15 @@
 #ifndef SQL_H
 #define SQL_H
-
+class QSqlQuery;
 class QSqlDatabase;
 class sql
 {
 public:
     sql();
     QSqlDatabase *db;
+    QSqlQuery *query;
     bool creatUser();
+    bool creatUser(int);
 };
 
 #endif // SQL_H
