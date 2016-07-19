e02f296bdeb5872a5e245ac65669c965c168b32c
gaoshashao <1723954627@qq.com>
2016-07-19 21:32:28 +0800

    取号端ui

--- getnum/close.cpp
+++ getnum/close.cpp
@@ -1,12 +1,18 @@
 #include "close.h"
 #include "ui_close.h"
 #include <QMessageBox>
+#include <QPalette>
+#include <QBrush>
 
 Close::Close(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Close)
 {
     ui->setupUi(this);
+    QPalette palette;
+    palette.setBrush(this->backgroundRole(),QBrush(QPixmap("../img/zx.jpg")));  //括号内为图片的相对目录
+    this->setPalette(palette);
+
 }
 
 Close::~Close()
